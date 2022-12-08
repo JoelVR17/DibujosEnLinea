cee7dc5095d7e6013b649602c035036375bca7c3
JoelVR17 <vermudo.com@gmail.com>
2022-12-08 12:00:58 -0600

    Funcion Creada

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,111 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useCallback, useRef, useState, useEffect } from "react";
+import "./App.css";
+import Footer from "./Footer";
+
+const colors = ["red", "green", "yellow", "black", "blue", "white", "brown", "pink", "gray"];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const canvasRef = useRef(null);
+  const ctx = useRef(null);
+
+  const [selectedColor, setSelectedColor] = useState(colors[0]);
+  const [mouseDown, setMouseDown] = useState(false);
+  const [lastPosition, setPosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      ctx.current = canvasRef.current.getContext("2d");
+    }
+  }, []);
+
+  const draw = useCallback((x, y) => {
+    if (mouseDown) {
+      ctx.current.beginPath();
+      ctx.current.strokeStyle = selectedColor;
+      ctx.current.lineWidth = 10;
+      ctx.current.lineJoin = "round";
+      ctx.current.moveTo(lastPosition.x, lastPosition.y);
+      ctx.current.lineTo(x, y);
+      ctx.current.closePath();
+      ctx.current.stroke();
+
+      setPosition({
+        x,
+        y,
+      });
+    }
+  }, [lastPosition, mouseDown, selectedColor, setPosition]);
+
+  const download = async () => {
+    const image = canvasRef.current.toDataURL('image/png')
+    const blob = await(await fetch(image)).blob()
+    const blobURL = URL.createObjectURL(blob)
+    const link = document.createElement('a')
+    link.href = blobURL
+    link.download = "image.png"
+    link.click()
+  }
+
+  const clear = () => {
+    ctx.current.clearRect(0, 0, ctx.current.canvas.width, ctx.current.canvas.height)
+  }
+
+  const onMouseDown = (e) => {
+    setPosition({
+      x: e.pageX,
+      y: e.pageY,
+    })
+    setMouseDown(true)
+  }
+
+  const onMouseUp = (e) => {
+    setMouseDown(false)
+  }
+
+  const onMouseMove = (e) => {
+    draw(e.pageX, e.pageY)
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <h1 className="titulo">Dibuja en Línea</h1>
+      <div className="contenedor__canvas">
+        <canvas 
+          className="canvas"
+          width={window.innerWidth - 60}
+          height={500}
+          ref={canvasRef}
+          onMouseDown={onMouseDown}
+          onMouseUp={onMouseUp}
+          onMouseLeave={onMouseUp}
+          onMouseMove={onMouseMove}
+          brushRadius={1}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <br />
+
+      <div className="contenedor__btn">
+        <select
+          className="btn colors"
+          value={selectedColor}
+          onChange={(e) => setSelectedColor(e.target.value)}
+        >
+          {colors.map((color) => (
+            <option key={color} value={color}>{color}</option>
+          ))}
+        </select>
+
+        <button className="btn borrar" onClick={clear}>Borrar</button>
+
+        <button className="btn descargar" onClick={download}>Descargar</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        <p className="footer__parrafo">Joel Vargas - © 2022</p>
+    </footer>
+  )
+}
+
+export default Footer
